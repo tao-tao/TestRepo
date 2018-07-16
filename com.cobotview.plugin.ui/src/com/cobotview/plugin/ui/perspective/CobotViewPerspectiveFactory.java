a2f858cfb31767f50c1d369ad9e4137bb00d5066
Tao Tao <taolongchina@gmail.com>
2018-07-16 15:52:01 +0800

    Unnecessary toolbar menu have been hidden.

--- com.cobotview.plugin.ui/src/com/cobotview/plugin/ui/perspective/CobotViewPerspectiveFactory.java
+++ com.cobotview.plugin.ui/src/com/cobotview/plugin/ui/perspective/CobotViewPerspectiveFactory.java
@@ -2,14 +2,33 @@ package com.cobotview.plugin.ui.perspective;
 
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
+import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.internal.Workbench;
-import org.eclipse.ui.internal.WorkbenchPage;
+import org.eclipse.ui.internal.WorkbenchIntroManager;
+import org.eclipse.ui.internal.WorkbenchWindow;
+import org.eclipse.ui.intro.IIntroPart;
 
 public class CobotViewPerspectiveFactory implements IPerspectiveFactory {
 
 	@Override
 	public void createInitialLayout(IPageLayout layout) {
+		IWorkbenchWindow[] windows = PlatformUI.getWorkbench().getWorkbenchWindows();
+
+		for( IWorkbenchWindow window : windows )
+		{
+//			if( window instanceof WorkbenchWindow )
+//			{
+//				WorkbenchIntroManager introManager = (WorkbenchIntroManager) window.getWorkbench().getIntroManager();
+//				
+//				IIntroPart introPart = introManager.getIntro();
+//				
+//				if( introPart == null )
+//				{
+//					introPart = introManager.showIntro(window, true);
+//					introManager.closeIntro(introPart);
+//				}
+//			}
+		}
 //		WorkbenchPage page = (WorkbenchPage) workbench.getActiveWorkbenchWindow().getActivePage();
 //
 //		page.hideActionSet("org.eclipse.ui.externaltools.ExternalToolMenuDelegateToolbar");
