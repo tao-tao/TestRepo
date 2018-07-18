2531230cd040314be03c061075712148b5237b99
Tao Tao <taolongchina@gmail.com>
2018-07-18 08:59:17 +0800

    Added Project Explorer to the Project View

--- com.cobotview.plugin.ui/src/com/cobotview/plugin/ui/perspective/CobotViewPerspectiveFactory.java
+++ com.cobotview.plugin.ui/src/com/cobotview/plugin/ui/perspective/CobotViewPerspectiveFactory.java
@@ -1,11 +1,16 @@
 package com.cobotview.plugin.ui.perspective;
 
+import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
+import org.eclipse.ui.navigator.resources.ProjectExplorer;
 
 public class CobotViewPerspectiveFactory implements IPerspectiveFactory {
 
 	@Override
 	public void createInitialLayout(IPageLayout layout) {
+ 		String editorArea = layout.getEditorArea();
+		IFolderLayout project= layout.createFolder("com.cobotview.plugin.ui.projectView", IPageLayout.LEFT, (float)0.25, editorArea); //$NON-NLS-1$
+		project.addView(ProjectExplorer.VIEW_ID);
 	}
 }
